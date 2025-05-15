2a6a565167ed9a7fae4ccaa5adb5967de05c5dd0
Uma Prasad Das <umaprasaddas1999@gmail.com>
2025-05-15 10:58:35 +0530

    Entity Creation

--- src/main/java/com/personal/backend/BackendApplication.java
+++ src/main/java/com/personal/backend/BackendApplication.java
@@ -1,12 +1,20 @@
 package com.personal.backend;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Objects;
 
 @SpringBootApplication
 public class BackendApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        // Set as system properties for Spring Boot to use
+        System.setProperty("DB_USERNAME", Objects.requireNonNull(dotenv.get("DB_USERNAME")));
+        System.setProperty("DB_PASSWORD", Objects.requireNonNull(dotenv.get("DB_PASSWORD")));
+
         SpringApplication.run(BackendApplication.class, args);
     }
 }

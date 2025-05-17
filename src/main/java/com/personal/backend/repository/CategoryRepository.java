101c120fbf76863c268c48037bdccd5b3301e982
Uma Prasad Das <umaprasaddas1999@gmail.com>
2025-05-17 10:47:03 +0530

    Implementing the Category Service.

--- src/main/java/com/personal/backend/repository/CategoryRepository.java
+++ src/main/java/com/personal/backend/repository/CategoryRepository.java
@@ -16,8 +16,6 @@ import com.personal.backend.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
-    Category finByName(String name);
-
     Category findByName(String name);
 
     boolean existsByName(String name);

a7cd403d2412f5f72914b0c739a311b7d58d0816
Uma Prasad Das <umaprasaddas1999@gmail.com>
2025-05-19 13:06:50 +0530

    Implementing the Controllers

--- src/main/java/com/personal/backend/BackendApplication.java
+++ src/main/java/com/personal/backend/BackendApplication.java
@@ -9,7 +9,6 @@ Importance: Boots the entire app using embedded Tomcat and starts auto-configura
  */
 package com.personal.backend;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -17,14 +16,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackendApplication {
 
     public static void main(String[] args) {
-        // Spring Boot automatically maps environment variables to properties.
-        // Ensure that DB_USERNAME and DB_PASSWORD are set in the environment or application.properties.
-        String dbUsername = System.getenv("DB_USERNAME");
-        String dbPassword = System.getenv("DB_PASSWORD");
-
-        if (dbUsername == null || dbPassword == null) {
-            throw new RuntimeException("Required environment variables DB_USERNAME or DB_PASSWORD are missing.");
-        }
         SpringApplication.run(BackendApplication.class, args);
     }
 }

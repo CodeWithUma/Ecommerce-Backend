4331d40280cf97c652ccb7ea4ef7af0483ef8609
Uma Prasad Das <umaprasaddas1999@gmail.com>
2025-05-24 08:53:01 +0530

    Implementing the Cart Service.

---
+++ src/main/java/com/personal/backend/service/cart/ICartService.java
@@ -0,0 +1,11 @@
+package com.personal.backend.service.cart;
+
+import com.personal.backend.model.Cart;
+
+import java.math.BigDecimal;
+
+public interface ICartService {
+    Cart getCart(Long id);
+    void clearCart(Long id);
+    BigDecimal getTotalPrice(Long id);
+}

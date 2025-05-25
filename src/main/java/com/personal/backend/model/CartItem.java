129a3763e9c1a33b9f86d7e837bc65ae45c2b58b
Uma Prasad Das <umaprasaddas1999@gmail.com>
2025-05-25 14:30:45 +0530

    Some Bug Fixes.

--- src/main/java/com/personal/backend/model/CartItem.java
+++ src/main/java/com/personal/backend/model/CartItem.java
@@ -20,7 +20,7 @@ public class CartItem {
     private int quantity;
     private BigDecimal unitPrice;
     private BigDecimal totalPrice;
-
+    
     @ManyToOne
     @JoinColumn(name = "product_id")
     private Product product;
@@ -30,11 +30,7 @@ public class CartItem {
     @JoinColumn(name = "cart_id")
     private Cart cart;
 
-    public static BigDecimal calculateTotalPrice(BigDecimal unitPrice, int quantity) {
-        return unitPrice.multiply(BigDecimal.valueOf(quantity));
-    }
-
-    public void updateTotalPrice() {
-        this.totalPrice = calculateTotalPrice(this.unitPrice, this.quantity);
+    public void setTotalPrice() {
+        this.totalPrice = this.unitPrice.multiply(new BigDecimal(quantity));
     }
 }

42459104d4635aa948509ebe8f031d3b2182fe9e
Uma Prasad Das <umaprasaddas1999@gmail.com>
2025-06-19 21:46:46 +0530

    Implementing Login Feature.

---
+++ src/main/java/com/personal/backend/request/LoginRequest.java
@@ -0,0 +1,13 @@
+package com.personal.backend.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String password;
+}
